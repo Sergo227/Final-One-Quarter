a0f081677ea45134a0ae5e2c3cc5ce7d7f371d3b
Sergo227 <sergo.mergo.1998@inbox.ru>
2023-04-29 14:22:30 +0300

    Завершение работы

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine ("Итоговая работа");
+
+string[] array1 = new string[] {"1234", "1567", "2", "-2", "computer science"};
+string[] array2 = new string[array1.Length];
+
+    for (int i = 0; i < array1.Length; i++)
+    {if (array1[i].Length <= 3)
+    {
+        Console.Write(array1[i] + " ");
+    }
+}
